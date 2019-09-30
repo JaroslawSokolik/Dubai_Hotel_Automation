f689218cbebe59a9bbc0a3dc32bf13a206749725
Jarek <jaroslaw.sokolik@clicktrans.pl>
2019-09-30 16:39:55 +0200

    init

--- hotel_test.py
+++ hotel_test.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.maximize_window()
 driver.get("http://www.kurs-selenium.pl/demo/")
+
+driver.find_element_by_id("s2id_autogen8").click()
+driver.find_element_by_xpath("//div[@id='select2-drop']//input").send_keys("Dubai")
+driver.find_element_by_xpath("//div[contains(text(),', United Arab Emirates')]").click()
+
 
